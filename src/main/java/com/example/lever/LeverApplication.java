4809240c1142c314dfb34bd658b0bbcbc56bc7c3
krishna-bellamkonda <kanthkrishna02@gmail.com>
2025-03-07 12:55:40 +0530

    app.yml

--- src/main/java/com/example/lever/LeverApplication.java
+++ src/main/java/com/example/lever/LeverApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LeverApplication {
+public class
+LeverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LeverApplication.class, args);
